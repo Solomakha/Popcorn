8fefccaa758a35375aa89c37e0b6e88108494cd1
Dmitry Solomaha <soldmich1995@gmail.com>
2023-02-03 18:29:14 +0200

    Add some elements in main page. Add Alamofire.

---
+++ Popcorn/MainPageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainPageViewController.swift
+//  Popcorn
+//
+//  Created by Admin on 03.02.2023.
+//
+
+import UIKit
+import Alamofire
+
+class MainPageViewController: UIViewController {
+
+    @IBOutlet weak var segmentedController: UISegmentedControl!
+    
+    @IBOutlet weak var popularLabel: UILabel!
+    @IBOutlet weak var popularCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
